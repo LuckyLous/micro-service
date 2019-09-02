4f410b3d84ae4027dd542a956c9bb2231e7af543
lous <helloandhitom@gmail.com>
2019-09-02 22:26:39 +0800

    eureka cluster

---
+++ micro-service-eureka-server-2003/src/main/java/com/louis/EurekaApplication_2003.java
@@ -0,0 +1,19 @@
+package com.louis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 主要是加入@EnableEurekaServer
+ * @author lu
+ * @create 2019-05-05 0:11
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication_2003 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication_2003.class, args);
+    }
+}
